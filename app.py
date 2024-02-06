7f4c08c79ec4d39630e2998995cbedd9c499591a
Madhan Chandrasekharan <32620038+gcmadhan@users.noreply.github.com>
2024-02-06 12:36:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
-import pandas_profiling
+#import pandas_profiling
 from pycaret.classification import *
 from sklearn.datasets import load_iris
 
